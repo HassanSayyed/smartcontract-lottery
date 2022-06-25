4228951e97b99326fdfded5859f0a00d7c00c732
Hasan Al Sayyed <hasanalsayyed651998@gmail.com>
2022-06-25 13:08:05 +0300

    3.0 adding tests

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -1,13 +1,86 @@
-import web3
-from brownie import Lottery, accounts, config, network
+from scripts.helpful_scripts import (
+    get_account,
+    fund_with_link,
+    get_contract,
+)
+from brownie import exceptions
 from web3 import Web3
+import pytest
 
-# current eth price is 1068$
-# 0.046
-# ~4600000000000000000
 
-def test_get_entrance_fee():
-    account = accounts[0]
-    lottery = Lottery.deploy(config["networks"][network.show_active()]["eth_usd_price_feed"] ,{"from":account})
-    # lottery.getEntranceFee() > Web3.toWei(0.044, "ether")
-    # lottery.getEntranceFee() > Web3.toWei(0.048, "ether")
+def test_get_entrance_fee(check_local_blockchain_envs, lottery_contract):
+    # Arrange (by fixtures)
+
+    # Act
+    # 1,097 eth / usd
+    # usdEntryFee is 50
+    # 1097/1 == 50/x == 0.045
+    expected_entrance_fee = Web3.toWei(0.045, "ether")
+    entrance_fee = lottery_contract.getEntranceFee()
+    # Assert
+    assert expected_entrance_fee == entrance_fee
+
+
+def test_cant_enter_unless_started(check_local_blockchain_envs, lottery_contract):
+    # Arrange (by fixtures)
+
+    # Act / Assert
+    with pytest.raises(exceptions.VirtualMachineError):
+        lottery_contract.enter(
+            {"from": get_account(), "value": lottery_contract.getEntranceFee()}
+        )
+
+
+def test_can_start_and_enter_lottery(check_local_blockchain_envs, lottery_contract):
+    # Arrange (by fixtures)
+
+    account = get_account()
+    lottery_contract.startLottery({"from": account})
+    # Act
+    lottery_contract.enter(
+        {"from": account, "value": lottery_contract.getEntranceFee()}
+    )
+    # Assert
+    assert lottery_contract.players(0) == account
+
+
+def test_can_end_lottery(check_local_blockchain_envs, lottery_contract):
+    # Arrange (by fixtures)
+
+    account = get_account()
+    lottery_contract.startLottery({"from": account})
+    lottery_contract.enter(
+        {"from": account, "value": lottery_contract.getEntranceFee()}
+    )
+    fund_with_link(lottery_contract)
+    lottery_contract.endLottery({"from": account})
+    assert lottery_contract.lottery_state() == 2
+
+
+def test_can_pick_winner_correctly(check_local_blockchain_envs, lottery_contract):
+    # Arrange (by fixtures)
+
+    account = get_account()
+    lottery_contract.startLottery({"from": account})
+    lottery_contract.enter(
+        {"from": account, "value": lottery_contract.getEntranceFee()}
+    )
+    lottery_contract.enter(
+        {"from": get_account(index=1), "value": lottery_contract.getEntranceFee()}
+    )
+    lottery_contract.enter(
+        {"from": get_account(index=2), "value": lottery_contract.getEntranceFee()}
+    )
+    fund_with_link(lottery_contract)
+    starting_balance_of_account = account.balance()
+    balance_of_lottery = lottery_contract.balance()
+    transaction = lottery_contract.endLottery({"from": account})
+    request_id = transaction.events["RequestedRandomness"]["requestId"]
+    STATIC_RNG = 777
+    get_contract("vrf_coordinator").callBackWithRandomness(
+        request_id, STATIC_RNG, lottery_contract.address, {"from": account}
+    )
+    # 777 % 3 = 0
+    assert lottery_contract.recentWinner() == account
+    assert lottery_contract.balance() == 0
+    assert account.balance() == starting_balance_of_account + balance_of_lottery
